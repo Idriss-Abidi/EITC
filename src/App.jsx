72e54f7eb5d1ad0378585d8ed66e9663493f4e99
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-09-10 21:56:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div> 
 
-     <div id="FirstDiv" className="container mx-auto px-10 ">
+     <div id="FirstDiv" className="container mx-auto px-5 overflow-auto ">
      <Navbar />
      <Welcome/>
      <About />
