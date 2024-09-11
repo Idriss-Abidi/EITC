b39257ea04633cad9c206e2a14fef0fd957f625c
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-09-11 02:20:09 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 const About = () => {
   return (
     <div id="ABOUT" className="border-b border-neutral-900 pb-4">
-      <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-50}} transition={{duration:0.5}} className="my-20 text-center text-4xl">ABOUT <span className="text-neutral-500">US</span></motion.h1>
+      <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-50}} transition={{duration:0.5}} className="my-20 text-center text-4xl text-neon">ABOUT <span className="text-neutral-500 us-neon">US</span></motion.h1>
       <div className="flex flex-wrap">
       <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:0.5}} className="w-full lg:w-1/2 lg:p-8">
         <div className="flex items-center justify-center">
