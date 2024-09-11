8cf826f657d9f62fc192bea4b08b46cd298f315a
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-09-11 05:19:21 +0100

    commit

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -46,7 +46,7 @@ const Welcome = () => {
             <div className="w-full lg:w-1/2"> 
                 <div className="flex flex-col items-center lg:items-start">
                     <motion.h1 variants={container1(0)} initial="hidden" id="eitc" animate="visible" className="my-20 text-7xl lg:text-8xl">EITC</motion.h1>
-                    <motion.span variants={container1(0.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">ENSIAS IT CLUB</motion.span>
+                    <motion.span variants={container1(0.5)} initial="hidden" id="eitc" animate="visible" className="my-1 text-2xl lg:text-2xl">ENSIAS IT CLUB</motion.span>
                     <motion.div variants={container1(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">
                         {/* <WelcomeSection/> */}
                         {/* <motion.h1
