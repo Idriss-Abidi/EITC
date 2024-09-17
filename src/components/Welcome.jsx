ab8c60b1df7089eba4b039267f103ae32f9d892a
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-09-17 15:50:19 +0100

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -54,8 +54,8 @@ const Welcome = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2"> 
                 <div className="flex flex-col items-center lg:items-start">
-                    <motion.h1 variants={container1(0)} initial="hidden" id="eitc" animate="visible" className="my-20 pr-[30px] text-7xl lg:text-8xl">EITC</motion.h1>
-                    <motion.span variants={container1(0.5)} initial="hidden" id="eitcn" animate="visible" className="my-1 pr-[30px] text-2xl lg:text-2xl">ENSIAS IT CLUB</motion.span>
+                    <motion.h1 variants={container1(0)} initial="hidden" id="eitc" animate="visible" className="my-20 lg:pr-[10%] text-7xl lg:text-8xl">EITC</motion.h1>
+                    <motion.span variants={container1(0.5)} initial="hidden" id="eitcn" animate="visible" className="my-1 lg:pr-[10%] text-2xl lg:text-2xl">ENSIAS IT CLUB</motion.span>
                     <motion.div variants={container1(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">
                         {/* <WelcomeSection/> */}
                         {/* <motion.h1
@@ -91,7 +91,9 @@ const Welcome = () => {
           initial="initial"
           animate="animate"
           //className="mt-3 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent mb-4"
-        className="center text-extra-neon text-1xl lg:text-2xl"
+        //className="center text-extra-neon text-1xl lg:text-2xl"
+          id="eitcn"
+          className="center text-[0.5rem] md:text-[1rem] "
         >
           Where IT minds get creative
         </motion.h3>
