8d0392d4b8dbdf024cd0e3e7706f0f2087388613
Idriss EL Abidi <136001812+Idriss-Abidi@users.noreply.github.com>
2024-10-16 21:56:31 +0100

    Update ItHolic.jsx

--- src/components/ItHolic.jsx
+++ src/components/ItHolic.jsx
@@ -45,7 +45,7 @@ const ItHolic = () => {
                         {/* Top images */}
                         <div className="flex space-x-[10px] md:space-x-[40px] items-center mb-20">
                             {/* Image with gold neon */}
-                            <div className="flex flex-col items-center space-y-4 mt-[-20px]">
+                            <div className="flex flex-col items-center space-y-4">
   <motion.img
     src={itl1}
     alt="cp1"
